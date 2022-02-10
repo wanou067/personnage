2c358c4a4b4c1f4e015f8868c2f5edad27c7ca30
wanou067 <wanou067@gmail.com>
2022-02-10 15:07:48 +0100

    Ajout de l'histoire de Tom et Jerry et des fonctions d'attaque

--- jeupersonnage.java
+++ jeupersonnage.java
@@ -5,7 +5,29 @@ public class jeupersonnage {
 
 	public static void main(String args[]){
 
-		Personnage Sorcier = new Personnage("enzo", 200);
-		System.out.println(Sorcier.Lenom());
+		String a = System.lineSeparator();
+
+		Personnage chat = new Personnage("Tom", 200, 100);
+		Personnage souris = new Personnage("Jerry", 50, 300);
+
+		System.out.println(a);
+		System.out.println("Il était une fois un chat qui s'appelait Tom qui avait 200HP et 100 points d'endurance");
+		System.out.println("Une souris nommée Jerry extremement jalouse était présente avec 50HP et 300 points d'endurance");
+
+		System.out.println(a + "Folle de jalousie la souris attaque le chat");
+		souris.damage(chat, 130);
+		System.out.println("Le chat est blessé, voici ses informations !");
+		System.out.println(chat.display());
+		System.out.println(souris.display());
+
+		System.out.println(a+"Assoifé de vengeance le chat saute sur la souris puis l'avale !");
+		chat.damage(souris, 50);
+		System.out.println(chat.display());
+		System.out.println(souris.display());
+		System.out.println("Jerry est morte ? " + souris.estMort());
+
+		System.out.println(a+"Le chat fier de son coup digère la souris et régénère 100 hp");
+		chat.heal(100);
+		System.out.println(chat.display());
 
 }}
